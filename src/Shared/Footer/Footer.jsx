d713686d7650a9d9681616ebffdbb1b42e52577a
sifat786 <solaimanislamsifat@gmail.com>
2024-06-07 18:33:38 +0600

    footer section done

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,11 +1,65 @@
+import { Typography } from "@material-tailwind/react";
+import logo from '../../assets/logo.png';
 
 
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <footer className="w-full bg-gray-200 p-8">
+      <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-gray-200 text-center md:justify-between">
+        <img
+          src={logo}
+          alt="logo-ct"
+          className="w-16"
+        />
+        <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
+          <li>
+            <Typography
+              as="a"
+              href="#"
+              color="black"
+              className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+            >
+              About Us
+            </Typography>
+          </li>
+          <li>
+            <Typography
+              as="a"
+              href="#"
+              color="black"
+              className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+            >
+              License
+            </Typography>
+          </li>
+          <li>
+            <Typography
+              as="a"
+              href="#"
+              color="black"
+              className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+            >
+              Contribute
+            </Typography>
+          </li>
+          <li>
+            <Typography
+              as="a"
+              href="#"
+              color="black"
+              className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+            >
+              Contact Us
+            </Typography>
+          </li>
+        </ul>
+      </div>
+      <hr className="my-8 border-[#075f47]" />
+      <Typography color="black" className="text-center font-normal">
+        &copy; 2024 Second Chance
+      </Typography>
+    </footer>
+  );
 };
 
 export default Footer;
